09db7e32fd911eacd6ff6e57f75c012d9539b946
Somnath-TCS <maji.somnath@tcs.com>
2026-06-01 06:26:27 +0000

    Finished ch1

--- src/Ch01/01_09b/CodeDemo.cpp
+++ src/Ch01/01_09b/CodeDemo.cpp
@@ -3,9 +3,14 @@
 // Terminal Interaction, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
 
 int main(){
-    std::cout << "Hi There!" << std::endl;
+    std::string name;
+
+    std::cout << "Enter your Name : " << std::flush;
+    std::cin >> name;
+    std::cout << "Nice to meet you " << name << " ! " << std::endl;
 
     std::cout << std::endl << std::endl;
     return 0;
